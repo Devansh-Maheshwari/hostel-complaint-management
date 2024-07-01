e49ba864aefab6aec4631dab1b95f0395581f515
Devansh-Maheshwari <caredevansh@gmail.com>
2024-07-02 03:59:52 +0530

    navbar

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -38,7 +38,7 @@ function Navbar() {
   return (
     <>
       <header className="shadow bg-white fixed w-full z-10 top-0">
-        <div className="relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center">
+        <div className="  flex max-w-screen-xl flex-row space-x-1 items-center  overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center">
           <a href="#" className="flex items-center whitespace-nowrap text-2xl font-black">
             <span className="text-black">ABV-IITM Hostel</span>
           </a>
@@ -46,15 +46,15 @@ function Navbar() {
          
           <nav
            
-            className={`peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col items-center justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start`}
+            className={`peer-checked:mt-8 peer-checked:max-h-56  flex  w-full flex-row items-center justify-between overflow-hidden transition-all  md:ml-24 md:max-h-full md:flex-row md:items-start`}
           >
-            <ul className="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
+            <ul className="flex flex-col items-center ml-5 space-y-0 md:ml-auto md:flex-row md:space-y-0">
               <li className="text-gray-600 md:mr-12 hover:text-blue-600">
                 <a href="/account">Account</a>
               </li>
               <li className="text-gray-600 md:mr-12 hover:text-blue-600">
                 <button
-                  className="rounded-md border-2 border-blue-600 px-6 py-1 font-medium text-blue-600 transition-colors hover:bg-blue-600 hover:text-white"
+                  className="rounded-md border-2 border-blue-600 px-1 py-0.5 font-medium text-blue-600 transition-colors hover:bg-blue-600 hover:text-white"
                   onClick={logout}
                 >
                   Logout
